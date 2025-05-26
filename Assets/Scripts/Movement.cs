13a2d577f780ad534607a72881d74210a920bbd1
Waffles24 <jennie.gibb21@gmail.com>
2025-05-26 05:10:53 +0100

    Art, Script, General
    Art :
    imported and setup tile maps
    begun setting up sprite order
    
    added running to blend tree + increased animation speed while running
    
    script :
    added running feature
    
    Engine
    begun creating colliders for sprites

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,8 @@ public class Movement : MonoBehaviour
      
     [SerializeField] private float Speed;
     [SerializeField] private Animator animator;
-
+    private Vector3 Direction;
+    private bool B_Was_Pressed;
 
     private void Update()
     {
@@ -23,25 +24,11 @@ public class Movement : MonoBehaviour
        float Vertical   = Input.GetAxisRaw("Vertical");
 
         // transform.position requires a vector 3 rather than 2
-        Vector3 Direction = new Vector3(Horizontal, Vertical);
+        Direction = new Vector3(Horizontal, Vertical);
 
         AnimateMovement(Direction);
 
-        /* "=" assigns the value on the right to the variable on the left.
-          "+=" adds the value on the right to the variable on the left, aswell as replacing it.
-         
-        E.g. where variable A = 2
-        A = 1 
-        will change the original value from 2 to 1. 
-
-        A += 1
-        will add 1 to the value of A : 1 + 2 = 3, so A would now be 3.*/
-
-       /* Time.deltaTime is really important for smoothing! Even with GetAxis, it's still jumpy.
-         with GetAxis the character seemed to keep moving after the key was let go - this could work
-        for an icy level, although for grass I want a faster response.
-        */
-        transform.position += Direction * Speed * Time.deltaTime;
+        
 
     }
 
@@ -61,10 +48,48 @@ public class Movement : MonoBehaviour
                 animator.SetBool("isMoving", false);
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            B_Was_Pressed = true;
+            animator.SetBool("isRunning", true);
+        }
+        if(Input.GetKeyUp(KeyCode.B))
+        {
+            B_Was_Pressed = false;
+            animator.SetBool("isRunning", false);
+        }
+
     }
 
 
 
 
+    private void FixedUpdate()
+    {
+        /* "=" assigns the value on the right to the variable on the left.
+          "+=" adds the value on the right to the variable on the left, aswell as replacing it.
+         
+        E.g. where variable A = 2
+        A = 1 
+        will change the original value from 2 to 1. 
+
+        A += 1
+        will add 1 to the value of A : 1 + 2 = 3, so A would now be 3.*/
+
+        /* Time.deltaTime is really important for smoothing! Even with GetAxis, it's still jumpy.
+          with GetAxis the character seemed to keep moving after the key was let go - this could work
+         for an icy level, although for grass I want a faster response.
+         */
+        transform.position += Direction * Speed * Time.deltaTime;
+
+        if (B_Was_Pressed)
+        {
+            //similar to above, *= will multiply the variable by the given value aswell as replace it
+            transform.position += Direction * Speed*1.7f * Time.deltaTime;
+        }
+
+
+    }
 
 }
